40665a3c01f2ad9a6924784a668d68520cb8480e
Wes Jonke <wesjonke@gmail.com>
2019-07-09 18:10:55 -0400

    initial commit, started adding to the buttons function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css";
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
+
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import "./App.css";
+
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,28 @@
-import React from "react";
-
-//import any components needed
-
+import React, { useState } from "react";
 //Import your array data to from the provided data file
+//import any components needed
+import { Numbers } from '../../../data'
 
-const Numbers = () => {
+
+
+
+
+
+export const Numbers = () => {
+
   // STEP 2 - add the imported data to state
+  const [ numbers, setNumbers ] = useState(numbers)
+
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      component matching the name on the provided file. Pass
+      it any props needed by the child component*/}
+      
+      {numbers.map(number => ( <button classname="button"> {number} </button> ))}
+
+      
     </div>
   );
 };
